152f2c4e1eab61f132ca00db8adda9f1e2522407
admin <admin@DESKTOP-SGDFQST>
2023-07-13 17:04:12 +0900

    hi

--- HelloWorld/src/co/yedam/ConditionExe.java
+++ HelloWorld/src/co/yedam/ConditionExe.java
@@ -4,7 +4,7 @@ public class ConditionExe {
 	public static void main(String[] args) {
 		//Math
 		for (int i=0; i < 10; i++){
-			int result = (int) (Math.random() * 10); // 0 ~ 10 사이의 값
+			int result = (int) (Math.random() * 6) + 1; // 0 ~ 6 사이의 값
 			System.out.println(result);
 	}
   }
