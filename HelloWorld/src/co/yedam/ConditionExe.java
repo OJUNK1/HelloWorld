45de7c43e995981d49ca7a32a88f127b7c490528
admin <admin@DESKTOP-SGDFQST>
2023-07-13 17:02:03 +0900

    hello

---
+++ HelloWorld/src/co/yedam/ConditionExe.java
@@ -0,0 +1,11 @@
+package co.yedam;
+
+public class ConditionExe {
+	public static void main(String[] args) {
+		//Math
+		for (int i=0; i < 10; i++){
+			int result = (int) (Math.random() * 10); // 0 ~ 10 사이의 값
+			System.out.println(result);
+	}
+  }
+}	
